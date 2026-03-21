4cd9a23d1a6fbdcb9b75e1dc92f1540767903522
aqeell7 <md.aqeelahmed007@gmail.com>
2026-03-21 19:34:51 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-// vite.config.ts
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   // This tells Vite to prefix all asset paths with your repository name
   base: '/jenkins-modernizer-stats/', 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
